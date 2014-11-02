aa2d11bdc9804008f9d3324f9941670f3119b1c8
nerdfiles <nerdfiles@gmail.com>
2014-11-02 17:31:55 -0600

    Cleanup.

---
+++ wp-content/themes/gos/search.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * gos template for displaying Search-Results-Pages
+ *
+ * @package WordPress
+ * @subpackage gos
+ * @since gos 1.0
+ */
+
+get_header(); ?>
+
+	<section class="page-content primary" role="main"><?php
+
+		if ( have_posts() ) : ?>
+
+			<div class="search-title">
+				<h1 ><?php printf( __( 'Search Results for: %s', 'gos' ), get_search_query() ); ?></h1>
+
+				<div class="second-search">
+					<p>
+						<?php _e( 'Not what you searched for? Try again with some different keywords.', 'gos' ); ?>
+					</p>
+
+					<?php get_search_form(); ?>
+				</div>
+			</div><?php
+
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'loop', get_post_format() );
+
+				wp_link_pages(
+					array(
+						'before'           => '<div class="linked-page-nav"><p>' . sprintf( __( '<em>%s</em> is separated in multiple parts:', 'gos' ), get_the_title() ) . '<br />',
+						'after'            => '</p></div>',
+						'next_or_number'   => 'number',
+						'separator'        => ' ',
+						'pagelink'         => __( '&raquo; Part %', 'gos' ),
+					)
+				);
+
+			endwhile;
+
+		else :
+
+			get_template_part( 'loop', 'empty' );
+
+		endif; ?>
+
+		<div class="pagination">
+
+			<?php get_template_part( 'template-part', 'pagination' ); ?>
+
+		</div>
+	</section>
+
+<?php get_footer(); ?>
