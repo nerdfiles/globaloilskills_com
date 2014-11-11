be6b2efe44cb886d235a48434eea621bc2e74237
nerdfiles <nerdfiles@gmail.com>
2014-11-10 23:32:03 -0600

    Added Controller.

---
+++ wp-content/plugins/json-api/controllers/make.php
@@ -0,0 +1,29 @@
+<?php
+
+class JSON_API_Make_Controller {
+
+  public function user() {
+
+    global $json_api;
+    $email = $json_api->query->email;
+
+    if ( email_exists($email) == false ) {
+
+        $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
+
+        $user_id = wp_create_user( $email, $random_password, $email );
+
+    } else {
+
+        $random_password = __('User already exists.  Password inherited.');
+
+    }
+
+    return array(
+      "user" => $user_id
+    );
+  }
+
+}
+
+?>
