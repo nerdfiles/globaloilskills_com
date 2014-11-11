cffed2737f2b35b6925b742fc98bc48a4386e89d
nerdfiles <nerdfiles@gmail.com>
2014-11-10 19:19:11 -0600

    PhoneGap work.

--- wp-content/themes/gos/grunt/gruntfile.js
+++ wp-content/themes/gos/grunt/gruntfile.js
@@ -112,6 +112,80 @@ module.exports = function(grunt) {
 			}
 		},
 
+    cordovacli: {
+        options: {
+            path: 'myHybridAppFolder'
+        },
+        cordova: {
+            options: {
+                command: ['create','platform','plugin','build'],
+                platforms: ['ios','android'],
+                plugins: ['device','dialogs'],
+                path: 'myHybridAppFolder',
+                id: 'io.cordova.hellocordova',
+                name: 'HelloCordova'
+            }
+        },
+        create: {
+            options: {
+                command: 'create',
+                id: 'com.myHybridApp',
+                name: 'myHybridApp'
+            }
+        },
+        add_platforms: {
+            options: {
+                command: 'platform',
+                action: 'add',
+                platforms: ['ios', 'android']
+            }
+        },
+        add_plugins: {
+            options: {
+                command: 'plugin',
+                action: 'add',
+                plugins: [
+                    'battery-status',
+                    'camera',
+                    'console',
+                    'contacts',
+                    'device',
+                    'device-motion',
+                    'device-orientation',
+                    'dialogs',
+                    'file',
+                    'geolocation',
+                    'globalization',
+                    'inappbrowser',
+                    'media',
+                    'media-capture',
+                    'network-information',
+                    'splashscreen',
+                    'vibration'
+                ]
+            }
+        },
+        build_ios: {
+            options: {
+                command: 'build',
+                platforms: ['ios']
+            }
+        },
+        build_android: {
+            options: {
+                command: 'build',
+                platforms: ['android']
+            }
+        },
+        emulate_android: {
+            options: {
+                command: 'emulate',
+                platforms: ['android'],
+                args: ['--target','Nexus5']
+            }
+        }
+    },
+
 		// check your php
 		phpcs: {
 			application: {
