16aba7f8eb2614c03a8c78f8a163d9d3195243fd
nerdfiles <nerdfiles@gmail.com>
2014-11-27 03:40:37 -0600

    Cleanup.

--- wp-content/themes/gos/header.php
+++ wp-content/themes/gos/header.php
@@ -42,7 +42,7 @@
 
     ?></title>
     <meta name="viewport" content="width=device-width" />
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" />
+    <link rel="shortcut icon" href="favicon.png" />
     <!--[if lt IE 9]>
       <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
     <![endif]-->
