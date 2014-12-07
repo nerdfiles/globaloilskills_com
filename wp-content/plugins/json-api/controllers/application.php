764be2fde0d672246f686de4cbd7c93933c001f0
nerdfiles <nerdfiles@gmail.com>
2014-12-07 15:01:37 -0600

    Cleanup.

--- wp-content/plugins/json-api/controllers/application.php
+++ wp-content/plugins/json-api/controllers/application.php
@@ -1,8 +1,39 @@
 <?php
 class JSON_API_Application_Controller {
   public function application_metadata() {
+    require_once  $_SERVER["DOCUMENT_ROOT"]."/wp-load.php";
+    /*
+     *ini_set('display_errors', 1);
+     *error_reporting('E_ALL');
+     */
+
+    $user_id = get_current_user_id();
+
+    /**
+     * So here we make an application that represents an employee user's submission to a particular job posting.
+     * We're using restful API methods exposed from WP's custom post taxonomy, and it's all very straightforward and simple. Oh, and fuck you.
+     */
+
+    $application_submission = array(
+      // The post title should be a template model that combines:
+      // 1. Username
+      // 2. Job posting title
+      // 3. Primary category (industry, I guess?)
+      'post_title'    => wp_strip_all_tags( $_POST['post_title'] ),
+      // If a resumé exists, we should recapitulate the resumé content (if parsable) into the post_content, otherwise, we take:
+      // the Contact Form 7 Template answers and reproduce them. This should be a cumulative process.
+      'post_content'  => $_POST['post_content'],
+      // The natural post status semantics don't make any fucking sense here. What does it mean to "publish" to an internal ecosystem of documents, you fucking prick?
+      'post_status'   => 'publish',
+      // The user ID should match the candidate to the recruiter or company that has posted the job posting. The cmpany should be notified via e-mail template post.
+      'post_author'   => $user_id,
+      // We should not use tags, but rather categories which are filled or informed by the relavant category list of the initial job posting combined with an intersection of the company categories.
+      'tags_input'    => implode(",","TAG1,TAG2"),//$_POST['tags']
+    );
+
+    $post_ID = wp_insert_post( $application_submission );
     return array(
-      "message" => "Hello!"
+      "application_submission" => $post_ID
     );
   }
 }
